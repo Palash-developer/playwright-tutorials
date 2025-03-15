b542803683e5fca9820027c8c8faff78331037d6
Palash-developer <duttabanikpalash7@gmail.com>
2025-03-15 14:42:47 +0530

    initial code commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,54 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+import { v4 as uuidv4 } from 'uuid';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+let username;
+let password;
+
+test.beforeAll(async () => {
+  username = `Palash_${uuidv4()}`;
+  password = `Palash@123_${uuidv4()}`;
+  console.log(`Generated username -> Password: ${username} -> ${password}`);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test.beforeEach(async ({page})=>{
+  await page.goto("/");
 });
+test("Has title STORE", async ({page}) => {
+  const pageTitle = await page.title();
+  console.log(pageTitle);
+  expect(pageTitle).toBe("STORE");
+})
+
+/*
+    Interacting with locators in Playwright:
+    There are 2 approaches to do this.
+        Approach 1: 
+          await page.locator('locator').click();
+        Approach 2:
+          await page.click('locator');
+*/
+
+test("Sign up for a new user", async ({page}) => {
+  await page.click('#signin2');
+  const signUpModalLabel =  page.locator('#signInModalLabel');
+  expect(signUpModalLabel).toHaveText("Sign up");
+  await page.fill('#sign-username', username);
+  await page.fill('#sign-password', password);
+  await page.locator('.btn-primary').filter({hasText: 'Sign up'}).click();
+  page.on('dialog', async (dialog) => {
+    console.log(`Alert message: ${dialog.message()}`);
+    await dialog.accept(); // Accept the alert
+  });
+})
+
+
+test("Login to the application", async({page}) =>{
+  await page.click('#login2');
+  const logInModalLabel =  page.locator('#logInModalLabel');
+  expect(logInModalLabel).toHaveText("Log in");
+  await page.fill('#loginusername', username);
+  await page.fill('#loginpassword', password);
+  await page.locator('.btn-primary').filter({hasText: 'Log in'}).click();
+})                                                 
